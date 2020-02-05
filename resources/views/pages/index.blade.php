33345f748dbe4c83d9eb72526e1b642e1324bd05
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-05 17:54:30 +0545

    Pages added

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+@extends('layouts.app')
+    <style>
+        *{
+            background: radial-gradient(circle, #3f87a6, #f69d3c);
+            
+        }
+        h1{
+            text-align: center;
+        }
+    </style>
+    <body>
+        <h1>Welcome to Laravel Zone</h1>
+        <p>This is the laravel application from scratch</p>
+    </body>
+</html>
+{{-- @section('content') --}}
+    
+{{-- @endsection --}}
