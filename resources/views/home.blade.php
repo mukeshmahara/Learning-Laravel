1b37936c5787da44a8655692848b1423dde35654
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-05 12:32:07 +0545

    added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home </title>
+</head>
+<body>
+<style>
+    *{
+        background: radial-gradient(circle, #3f87a6, #f69d3c);
+        
+    }
+    h1{
+        text-align: center;
+    }
+</style>
+    <h1>Welcome to Laravel zone</h1>
+    
+</body>
+</html>
