c7f6cd462627c3ebb3b3b8f0f26afaa88c94f56d
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-06 07:42:50 +0545

    layout added in index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,26 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
 @extends('layouts.app')
-    <style>
-        *{
-            background: radial-gradient(circle, #3f87a6, #f69d3c);
-            
-        }
-        h1{
-            text-align: center;
-        }
-    </style>
-    <body>
-        <h1>Welcome to Laravel Zone</h1>
-        <p>This is the laravel application from scratch</p>
-    </body>
-</html>
-{{-- @section('content') --}}
-    
-{{-- @endsection --}}
+<style>
+    *{
+        background: radial-gradient(circle, #3f87a6, #f69d3c);
+        
+    }
+    h1{
+        text-align: center;
+    }
+</style>
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application from scratch</p>
+@endsection
