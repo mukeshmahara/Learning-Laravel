c8007281ca9e68d7adec4470a97e6d05fb90e4b9
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-07 09:26:12 +0545

    uppdated app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>{{config('app.name','myapp')}}</title>
+    {{-- <link rel="stylesheet" href="C:\xampp\htdocs\myapp\node_modules\bootstrap\dist\css\bootstrap.min.css"> --}}
+    <link rel="stylesheet" href="{{ asset('sass/custom.scss') }}" type="text/css">
+    <title>{{config('app.name','myapp')}}</title>
 </head>
 <body>
-
-    @yield('content') 
+    @include('inc.navbar')
+    <div class="container">
+        
+        @yield('content') 
     
+
+    </div>
 </body>
 </html>
