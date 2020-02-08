ce221877094b74b0ae0b4cbac474936fa79ffe70
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-08 22:50:36 +0545

    bootstrap CDN added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- <link rel="stylesheet" href="C:\xampp\htdocs\myapp\node_modules\bootstrap\dist\css\bootstrap.min.css"> --}}
-    <link rel="stylesheet" href="{{ asset('sass/custom.scss') }}" type="text/css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>{{config('app.name','myapp')}}</title>
 </head>
 <body>
     @include('inc.navbar')
     <div class="container">
-        
+
         @yield('content') 
     
-
     </div>
+
+    
 </body>
 </html>
