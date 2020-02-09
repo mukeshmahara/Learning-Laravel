460ae64a16bdc89c1c14ce2c240ae933befaf09e
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 22:47:33 +0545

    css modification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,10 +17,7 @@
         @include('inc.messages')
     </div>
 
-    {{-- <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-    <script>
-        CKEDITOR.replace( 'article-ckeditor' ); --}}
-    {{-- </script> --}}
+    
 </body>
 </html>
  
