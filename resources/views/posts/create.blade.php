885109ff76c19db221777fe5617176952e646db2
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 22:49:08 +0545

    optimized create functionality

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create post</h1>
-    {!!Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
-        <div class="form-group">
-            {!!Form::label('title','Title')!!}
-            {!!Form::text('title','',['class'=>'form-control','placeholder'=>'Tilte' ])!!}
-        </div>
 
-        <div class="form-group">
-            {!!Form::label('body','Title')!!}
-            {!!Form::textarea('body','',['class'=>'form-control','placeholder'=>'body'])!!}
-        </div> 
+<div class="bg-success">
+    <h1 class="text-center"><strong>Create post</strong></h1>
 
-        {{Form::submit('Submimt',['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
-    
-    
-    
+</div>
+<hr>
+{!!Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+<div class="form-group">
+    {!!Form::label('title','Title')!!}
+    {!!Form::text('title','',['class'=>'form-control','placeholder'=>'Tilte' ])!!}
+</div>
+
+<div class="form-group">
+    {!!Form::label('body','Title')!!}
+    {!!Form::textarea('body','',['class'=>'form-control','placeholder'=>'body'])!!}
+</div>
+
+{{Form::submit('Submimt',['class'=>'btn btn-primary'])}}
+<a href="./" class="btn btn-warning"> Cancel</a>
+{!! Form::close() !!}
+
+
+
+
 @endsection()
