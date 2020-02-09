6c4d19e8301ba10a9e395a028f6f021506f0b5ce
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 23:23:41 +0545

    UI changed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,40 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Posts</h1>
 
-    @if (count($posts)> 0)
-        @foreach ($posts as $post)
-            {{-- <div class="well"> --}}
-            {{-- <div class="card" style="width: 18rem;">
+<div >
+  <a href="#" class="btn pull-right"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+  <a href="#" class="btn pull-right"><span class="glyphicon glyphicon-log-in "></span> Login</a>
+
+</div>
+<h1>All Posts</h1>
+<hr>
+
+
+<div>
+
+
+
+  @if (count($posts)> 0)
+    @foreach ($posts as $post)
+        {{-- <div class="well"> --}}
+        {{-- <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">{{$post->title}}</h5>
-                    <h3><a href="./{{$post->id}}" class="card-text">{{$post->body}}</a></h3>
-                    <small>Create at {{$post->created_at}}</small>
+        <h3><a href="./{{$post->id}}" class="card-text">{{$post->body}}</a></h3>
+        <small>Create at {{$post->created_at}}</small>
+        </div>
+      </div> --}}
 
-                </div>
-            </div> --}}
-            <div class="row">
-                <div class="col-sm-4">
-                  <div class="card bg-secondary mb-3">
-                    <div class="card-body well">
-                    <h3 class="card-title">{{$post->title}}</h3>
-                    <small>Create at {{$post->created_at}}</small>
-                    <p class="card-text">{{$post->body}}</p>
-                      <a href="./posts/{{$post->id}}" class="btn btn-primary">Read more</a>
-                      
-                    </div>
-                  </div>
-                </div>
-        @endforeach
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="card bg-secondary mb-3">
+            <div class="card-body well">
+              <h3 class="card-title">{{$post->title}}</h3>
+              <small>Create at {{$post->created_at}}</small>
+              <p class="card-text">{{$post->body}}</p>
+              <a href="./posts/{{$post->id}}" class="btn btn-primary">Read more</a>
 
-        {{$posts->links()}}
-         
+            </div>
+          </div>
+        </div>
+    @endforeach
+
+    {{$posts->links()}}
+
     @else
-        <P>No post found</P>
-        
-    @endif
+    <P>No post found</P>
+
+  @endif
+
+</div>
 
 @endsection
-
-
