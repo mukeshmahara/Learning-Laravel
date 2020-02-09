277dd3dd28119111ba10f0a37c925da6755b21f0
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 18:23:38 +0545

    error removed in index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,27 @@
 
     @if (count($posts)> 0)
         @foreach ($posts as $post)
-            <div class="well">
-            <h3><a href="./{{$post->id}}">{{$post->title}}</a></h3>
-            <small>Create at {{$post->created_at}}</small>
-            </div>
+            {{-- <div class="well"> --}}
+            {{-- <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$post->title}}</h5>
+                    <h3><a href="./{{$post->id}}" class="card-text">{{$post->body}}</a></h3>
+                    <small>Create at {{$post->created_at}}</small>
+
+                </div>
+            </div> --}}
+            <div class="row">
+                <div class="col-sm-4">
+                  <div class="card">
+                    <div class="card-body">
+                    <h3 class="card-title">{{$post->title}}</h3>
+                    <small>Create at {{$post->created_at}}</small>
+                    <p class="card-text">{{$post->body}}</p>
+                      <a href="./posts/{{$post->id}}" class="btn btn-primary">Read more</a>
+                      
+                    </div>
+                  </div>
+                </div>
         @endforeach
 
         {{$posts->links()}}
