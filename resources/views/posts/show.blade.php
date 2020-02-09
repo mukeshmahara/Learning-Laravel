ba080a200135a49ac6e8d8f2585b3ba784612e7c
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 23:24:06 +0545

    UI updated

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 
 
-<h1>{{$post->body}}</h1>
+<h1>{{$post->title}}</h1>
 <div class="row">
     <div class="col-sm-12">
-        <div class="card bg-seconadry mb-3">
+        <div class="card-block bg-seconadry mb-3">
             <div class="card-body well">
                 <h3 class="card-title">{{$post->title}}</h3>
 
                 <p class="card-text">{{$post->body}}</p>
 
-                <small class="text-center">Create at {{$post->created_at}}</small>
+                <small class="text-center">Created at {{$post->created_at}}</small>
 
 
 
@@ -21,10 +21,11 @@
             <div class="pull-right">
 
                 {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POSt'])!!}
-                <a href="./" class="btn btn-secondary ">Go Back</a>
-                <a href="./{{$post->id}}/edit" class="btn btn-dark">Edits</a>
+                
+                <a href="./{{$post->id}}/edit" class="btn btn-default">Edits</a>
                 {{ Form::hidden('_method','DELETE')}}
-                {{Form::submit('Delete',['class'=>'btn btn-danger pull-right'])}}
+                {{Form::submit('Delete',['class'=>'btn btn-default '])}}
+                <a href="./" class="btn btn-default ">Go Back</a>
                 {!!Form::close()!!}
                 <hr>
             </div>
