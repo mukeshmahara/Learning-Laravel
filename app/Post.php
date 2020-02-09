3aa9fcb689c679a202c17c2727f002dfb430916e
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 13:35:12 +0545

    edited post.blade.php

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Table Name
+    protected $table = 'posts';
+
+    //primary Key
+
+    public $primaryKey = 'id';
+
+    //Timestamps
+
+    public $timestamps = true;
 }
