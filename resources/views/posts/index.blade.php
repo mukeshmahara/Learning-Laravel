3bd14a886e23dc5673e0037144d20fd8ae9d0f01
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 13:49:08 +0545

    added navigation

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>This is the post page</h1>
+    <h1>Posts</h1>
 
     @if (count($posts)> 1)
         @foreach ($posts as $post)
             <div class="well">
-            <h3><a href="{{$post->id}}">{{$post->title}}</a></h3>
+            <h3><a href="./posts/{{$post->id}}">{{$post->title}}</a></h3>
             <small>Create at {{$post->created_at}}</small>
             </div>
         @endforeach
