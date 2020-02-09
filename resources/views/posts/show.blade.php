ac7593ac249bbeff2eea4e4a95af1d30cc4d06d9
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 22:23:57 +0545

    updated UI

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,40 @@
 @section('content')
 
 
-    <h1>{{$post->body}}</h1>
-    
-    <div class="btn btn-default"><a href="./">Go Back</a></div>
+<h1>{{$post->body}}</h1>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card bg-seconadry mb-3">
+            <div class="card-body well">
+                <h3 class="card-title">{{$post->title}}</h3>
+
+                <p class="card-text">{{$post->body}}</p>
+
+                <small class="text-center">Create at {{$post->created_at}}</small>
+
+
+
+            </div>
+             
+            <div class="pull-right">
+
+                {!!Form::open(['action'=>['PostsController@destroy',$post->id],'method'=>'POSt'])!!}
+                <a href="./" class="btn btn-secondary ">Go Back</a>
+                <a href="./{{$post->id}}/edit" class="btn btn-dark">Edits</a>
+                {{ Form::hidden('_method','DELETE')}}
+                {{Form::submit('Delete',['class'=>'btn btn-danger pull-right'])}}
+                {!!Form::close()!!}
+                <hr>
+            </div>
+
+
+        </div>
+
+
+
+    </div>
+</div>
+
+
 
 @endsection
