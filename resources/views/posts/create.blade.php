d403a5f34bf98bdea16bce3fe8302c6962407c3c
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 15:02:40 +0545

    added form and text area

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
     <h1>Create post</h1>
+    {!!Form::open(['action'=>'PostsController@store','method'=>'POST']) !!}
+        <div class="form-group">
+            {!!Form::label('title','Title')!!}
+            {!!Form::text('title','',['class'=>'form-control','placeholder'=>'Tilte'])!!}
+        </div>
 
-    <textarea name="" id="" cols="100" rows="10"></textarea>
+        <div class="form-group">
+            {!!Form::label('body','Title')!!}
+            {!!Form::textarea('body','',['class'=>'form-control','placeholder'=>'body'])!!}
+        </div>
+        
+        {{Form::submit('Submimt',['class'=>'btn btn-primary'])}}
+    {!! Form::close() !!}
+    
+    
     
 @endsection()
