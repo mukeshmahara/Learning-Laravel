2e1b96546f30358728d96c7dee94512d9c5a1f08
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-09 13:38:04 +0545

    modified navbars

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,7 +6,7 @@
       <a class="navbar-brand" href="">Learning Laravel</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="./">Home</a></li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Notice<span class="caret"></span></a>
         <ul class="dropdown-menu">
           <li><a href="#">Admision</a></li>
@@ -14,9 +14,11 @@
           <li><a href="#">Exams</a></li>
         </ul>
       </li>
-      <li><a href="#">Portfolio</a></li>
+      <li><a href="./portfolio">Portfolio</a></li>
       
-      <li><a href="#">Contact</a></li>
+      <li><a href="./about">About</a></li>
+      <li><a href="./posts">Blog</a></li>
+      
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
