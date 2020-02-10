e6b020bf74c5742cf2e61363d3839857b08ee147
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-10 16:05:51 +0545

    added userid for each post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -49,7 +49,7 @@ class PostsController extends Controller
 
         $post ->title = $request->input('title');
         $post -> body = $request->input('body');
-
+        $post ->user_id = auth()->user()->id;
         $post->save();
 
         return redirect('./posts')->with('sucess','Post Created');
