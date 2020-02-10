098c755fb1849d1e9f20e50a7cb6c304aacfc551
Mukesh Mahara <mukeshmahara1@gmail.com>
2020-02-10 17:36:23 +0545

    navigation corrected

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,32 +9,32 @@
 
 <div>
   @if (count($posts)> 0)
-    @foreach ($posts as $post)
-     
-      <div class="row">
-        <div class="col-sm-4">
-          <div class="card bg-secondary mb-3">
-            <div class="card-body well">
-              <h3 class="card-title">{{$post->title}}</h3>
-              <small>Create at {{$post->created_at}}</small>
-              <p class="card-text">{{$post->body}}</p>
-              <a href="./posts/{{$post->id}}" class="btn btn-primary">Read more</a>
+  @foreach ($posts as $post)
 
-            </div>
-          </div>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card bg-secondary mb-3">
+        <div class="card-body well">
+          <h3 class="card-title">{{$post->title}}</h3>
+          <small>Create at {{$post->created_at}} by {{$post->user->name}}</small>
+          <p class="card-text">{{$post->body}}</p>
+          <a href="./posts/{{$post->id}}" class="btn btn-primary">Read more</a>
+
         </div>
+      </div>
+    </div>
     @endforeach
-  
+
 
     {{$posts->links()}}
 
     @else
     <P>No post found</P>
 
-  @endif
-  
+    @endif
 
-</div>
-<hr>
 
-@endsection
+  </div>
+  <hr>
+
+  @endsection
